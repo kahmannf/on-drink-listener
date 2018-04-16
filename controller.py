edf91694dc174886d4d50640235660bff53d0e81
Felix Kahmann <felix@kahmann.de>
2018-04-16 11:17:01 +0200

    add controller.py

---
+++ controller.py
@@ -0,0 +1,36 @@
+from data import Data
+from sys import argv
+
+isMockController = True
+
+
+if not len(argv) > 1 or not argv[1] == 'no_controller':
+    import smbus2
+    import time
+    
+    # for RPI version 1, use "bus = smbus.SMBus(0)"
+    bus = smbus2.SMBus(1)
+
+    # This is the address we setup in the Arduino Program
+    address = 0x04
+
+    isMockController = False
+
+    def writeNumber(value):
+        bus.write_byte(address, value)
+        
+        return -1
+
+    def open_port(port):
+        writeNumber(port + 2)
+        writeNumber(1)
+
+    def close_port(port):
+        writeNumber(port + 2)
+        writeNumber(0)
+
+
+class Controller:
+    def mix_cocktail(self, cocktailname):
+        pass
+        
